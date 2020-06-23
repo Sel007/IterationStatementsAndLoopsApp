7bdc96b1b991f5f405a36ee406cb5841d9660d8d
Sel007 <selina.08225.1@gmail.com>
2020-06-23 13:49:17 -0400

    Original

--- IterationStatementsAndLoopsConsuleUI/Program.cs
+++ IterationStatementsAndLoopsConsuleUI/Program.cs
@@ -6,7 +6,36 @@ namespace IterationStatementsAndLoopsConsuleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 0;
+
+            //while (i < 10);
+            //{
+            //Console.WriteLine("Hello World!");
+            // i++;
+            //}
+
+            //do
+            //{
+            //Console.WriteLine("Hello World!");
+            //i++;
+            //}
+
+            int[] numbers = { 1, 2, 1, 2, 1, 2, };
+
+            //for (int j = 0; j < numbers.length; j++)
+
+            //    numbers[j] += 1;
+            //{
+            //    Console.WriteLine($"{numbers[j]} is the {j} index in the array");
+            //}
+
+            foreach (int number in numbers)
+            {
+                ConsoleWriteLine($"{numbers}");
+            }
+
+
+
         }
     }
 }
